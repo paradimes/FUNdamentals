ec48831684198f05f3573464cb4417ff1c9d153e
AM <test@test.com>
2023-08-10 23:33:22 -0400

    .env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
 
+const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID;
+const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain="dev-turylcy4n5nqgkyp.us.auth0.com"
-    clientId="iMu38THayXxSgNIX1qmloha9c6at7cj7"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: "http://localhost:5173/welcome",
     }}
